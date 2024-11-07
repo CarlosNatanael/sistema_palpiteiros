1f0aea5f3f87b4d5ad0f6794d129d434c2b19602
Srleo12 <carlosnatha2345@gmail.com>
2024-11-07 14:58:18 -0300

    Alteração no serividro

---
+++ Servidor.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Define o diretório do projeto Flask
+cd /d "C:\Users\SUPORTE 03\Desktop\EU\Eu (Carlos)\Cod_Dev\sistema_palpiteiros"
+
+REM Define a variável de ambiente para o arquivo Flask principal
+set FLASK_APP=app.py
+
+REM Define o ambiente como development para habilitar o modo debug
+set FLASK_ENV=development
+
+REM Inicia o servidor Flask no IP e porta especificados
+flask run --host=192.168.0.238 --port=5000
